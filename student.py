624fb9b670e5423b62a4667347b79c8c9b6f12b5
Никита <nikitasta.russia@gmail.com>
2026-09-11 20:36:40 +0300

    Добавил поле group

--- student.py
+++ student.py
@@ -1,6 +1,7 @@
 class Student:
-    def __init__(self, name, age):
+    def __init__(self, name, age, group):
         self.name = name
         self.age = age
+        self.group = group
     def get_Info(self):
-        return f"Имя: {self.name}\nВозраст: {self.age}"
+        return f"Имя: {self.name}\nВозраст: {self.age}\nГруппа: {self.group}"
